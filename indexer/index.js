ea28fe06dd052a1f14d2d771faf43432e5a481e9
Toni-d-e-v <toni.dumancic2@gmail.com>
2021-10-30 18:55:29 +0200

    from key

--- indexer/index.js
+++ indexer/index.js
@@ -1,222 +1,34 @@
 const start = async function() {
     // import needed libs
-    var MongoClient = require('mongodb').MongoClient;
     const fetch = require('node-fetch');
     var net = require('net');
     var gini = require("gini");
     var express = require("express");
     var cors = require('cors')
-        // connect to mongodb
-    var url = "mongodb://localhost:27017/";
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        // open the DB
-        var dbo = db.db("rsexplorer");
-
-        var client = new net.Socket(); // create a new socket to connect to the avrio-daemon RPC server
-        var init = false;
-        client.connect(44405, '127.0.0.1', function() {
-            console.log('Connected to rpc');
-            client.write('init'); // write first message to the daemon
-
-        });
-
-        var app = express(); // launch the API app
-        app.use(cors()) // open the API to everyone
-        var tot_json = `[`
-        app.listen(1234, () => {
-            console.log("API running on port 1234");
-        });
-
-
-        app.get("/lastten", (req, res, next) => { // gets the last ten blocks
-            let blocks_cursor = dbo.collection("blocks").find().sort({ _id: -1 }).limit(10);
-
-            blocks_cursor.toArray().then(array => {
-                res.json(array);
-            })
-        })
-
-        app.get("/chaincount", (req, res, next) => { // gets the number of chains/
-            fetch(`http://127.0.0.1:8000/api/v1/chainlist`)
-                .then(function(response) {
-                    return response.json();
-                })
-                .then(function(myJson) {
-                    if (myJson['success'] == true) {
-                        res.json({ count: myJson['list'].length })
-                    } else {
-                        console.log("Failed to get chain count from node")
-                        res.json({ count: 0 })
-                    }
-                })
-        })
-
-        app.get("/get_mem_tx/:hash", (req, res, next) => { // gets the number of chains/
-            try {
-            fetch(`http://127.0.0.1:8000/json_api/get_tx/${req.params.hash}`)
-            
-                .then(function(response) {
-                    if (response != null) {
-                    return response.json();
-                    }
-
-                })
-                .then(function(myJson) {
-                    if (myJson != null) {
-
-                    if (myJson['success'] == true) {
-                        res.json({ count: myJson['list'].length })
-                    } else {
-                        console.log("Failed to get chain count from node")
-                        res.json({ count: 0 })
-                    }
-                }
-                })
-            }
-            catch {
-                function resolved(result) {
-                    console.log('Resolved');
-                }
-                  
-                function rejected(result) {
-                    console.error(result);
-                }
-                  
-                Promise.reject(new Error('fail')).then(resolved, rejected);
-                
-            }
-        })
-        app.get("/get_acc/:hash", (req, res, next) => { // gets the number of chains/
-            try {
-            fetch(`http://127.0.0.1:8000/json_api/get_acc/${req.params.hash}`)
-                .then(function(response) {
-                    try {
-                    return response.json();
-                    }
-                    catch {
-                        res.status(401).json("Invalid credentials!")
-                    }
-                    console.log(1)
-
-                })
-                .then(function(myJson) {
-                    //0x9ec672b9957a57b6862056feb9d7c32903b5501f
-                    console.log(2)
-
-                        if (myJson['success'] == true) {
-                            console.log(3)
-                            try {
-                            res.json(myJson)
-                            }
-                            catch {
-                                res.status(401).json("Invalid credentials!")
-                            }
-                        } else {
-                            console.log(33)
-
-                            console.log("Failed to get chain count from node")
-                        }
-                    
-                    
-                })
-            }
-            catch {
-                console.log('That did not go well.')
-                res.status(401).json("Invalid credentials!")
-            }
-        })
-        app.get("/block/:hash", (req, res, next) => { // gets a block by its hash
-            let blocks_cursor = dbo.collection("blocks").find({ hash: req.params.hash });
-
-            blocks_cursor.toArray().then(array => {
-                if (typeof array[0] != 'undefined') {
-                    console.log(array[0]);
-                    res.json(array[0]);
-                } else {
-                    fetch(`http://127.0.0.1:8000/json_api/get_blk/${req.params.hash}`)
-                        .then(function(response) {
-                            return response.json();
-                        })
-                        .then(function(myJson) {
-                            console.log(myJson);
-                            if (myJson['success'] == true) {
-                                res.json(myJson['response']['block'])
-                            } else {
-                                console.log("Failed to get block from node")
-                                res.json([])
-                            }
-                        })
-                };
-            });
-        })
-        app.get("/tx/:hash", (req, res, next) => { // gets a block by its hash
-            let transactions_cursor = dbo.collection("transactions").find({ hash: req.params.hash });
-
-            transactions_cursor.toArray().then(array => {
-                if (typeof array[0] != 'undefined') {
-                    console.log(array[0]);
-                    res.json(array[0]);
-                } else {
-                    fetch(`http://127.0.0.1:8000/json_api/get_blk/${req.params.hash}`)
-                        .then(function(response) {
-                            return response.json();
-                        })
-                        .then(function(myJson) {
-                            console.log(myJson);
-                            if (myJson['success'] == true) {
-                                res.json(myJson['response']['block'])
-                            } else {
-                                console.log("Failed to get block from node")
-                                res.json([])
-                            }
-                        })
-                };
-            });
-        })
-        client.on('data', function(data) { // when we recieve data from the RPC
-            console.log('Received: ' + data);
-            if (!init) { // is this the first message recieved?
-                console.log("Not init, sending *");
-                client.write('*'); // register all services
-                init = true; // set init to true
-            } else { // we have already registered all services, parse the data
-                var rec = JSON.parse(data) // we only listen for blocks for now
-                if (rec.m_type == "block") {
-                    var block = JSON.parse(rec.content)
-                    dbo.collection("blocks").insertOne(block, function(err, res) { // save the block
-                        if (err) throw err;
-                        console.log(`1 block with hash=${block['hash']} inserted`);
-                    });
-                    // save each transaction in a block by its hash
-                    // get block transactions in to array
-                    //var txs = block['transactions']
-                    for (var i = 0; i < block['transactions'].length; i++) {
-                        console.log(block['transactions'][i]['hash'])
-                        var tx = block['transactions'][i];
-                        dbo.collection("transactions").insertOne(tx, function(err, res) {
-                            if (err) throw err;
-                            console.log(`1 tx with hash=${tx['hash']} inserted`);
-                        });
-                    }
-                    if (block.block_type = "Send") {
-                        let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.chain_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 } ] },`
-
-                        tot_json = tot_json + new_json
-                    } else {
-                        let new_json = `{hash:${block.hash}, time: ${block.header.timestamp}, public_key: ${block.header.chain_key}, links: [ {hash: ${block.header.prev_hash}, type: 0 }, {hash: ${block.send_block}, type: 1 } ] },`
-                        tot_json = tot_json + new_json
-                    }
-                } else {
-                    console.log(`Recieved non block msg, type=${rec.m_type}`)
-                }
-            }
-        });
-
-        client.on('close', function() {
-            console.log('rpc closed');
-        });
+    var app = express(); // launch the API app
+    app.use(cors()) // open the API to everyone
+    var tot_json = `[`
+    app.listen(1234, () => {
+        console.log("API running on port 1234");
     });
+    app.get("/create_wallet", (req, res, next) => { // gets the number of chains/
+        fetch(`http://127.0.0.1:8000/json_api/create_wallet`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                res.json(json);
+            })
+    })
+    app.get("/from_key/:key", (req, res, next) => { // gets the number of chains/
+        fetch(`http://127.0.0.1:8000/json_api/from_private_key/${req.params.key}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(json) {
+                res.json(json);
+            })
+    })
 }
 
   // expected output: Error: fail
--- src/App.js
+++ src/App.js
@@ -2,306 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function getblk(chain) {
-  fetch('http://127.0.0.1:1234/lastten')
+function CreateWallet() {
+  fetch('http://127.0.0.1:1234/create_wallet')
   .then(function(response) {
     console.log(response);
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson);
-    
-    show(myJson);
+    document.getElementById("wallet_address").innerHTML = myJson.address;
+    document.getElementById("wallet_private_key").innerHTML = myJson.private_key;
+    document.getElementById("wallet_public_key").innerHTML = myJson.public_key;
 
   });
   
 }
 
-function getacc(acc) {
-    fetch('http://0.0.0.0:1234/get_acc/' + acc)
-    .then(function(response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function(myJson) {
-      console.log(myJson);
-      show1(myJson);
-    });
-}
-function gettx(tx) {
-  fetch('http://0.0.0.0:1234/tx/' + tx)
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-    show2(myJson);
-  });
-}
 
-function getblkhash(acc) {
-  fetch('http://0.0.0.0:1234/block/' + acc)
-  .then(function(response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-    show3(myJson);
-  });
-}
-function show3(data) {
-  let tab = 
-  `<tr>
-  <th>Hash</th>
-  <th>Heigt</th>
-  <th>Chain</th>
-  <th>Parent hash</th>
-  <th>Uncle root</th>
-  </tr>
-`;
-
-      tab +=`
-        <tr> 
-          <td>${data.hash} </td>
-          <td>${data.header.height}</td>
-          <td>${data.header.chain}</td> 
-          <td>${data.header.parent_hash}</td> 
-          <td>${data.header.uncle_root}</td>  
-        </tr>`;
-  let tab_tx =
-  `<tr>
-    <th>From</th>
-    <th>Amount</th>
-    <th>To</th>
-    <th>Hash</th>
-  </tr>
-  `;
-  for (let r of data.transactions) {
-    tab_tx += `
-      <tr>
-        <td>${r.sender}</td>
-        <td>${r.amount}</td>
-        <td>${r.reciver}</td>
-        <td> <a href="?tx=${r.hash}"> ${r.hash}</a> </td>
-      </tr>
-    `;
-  }
-  
-  // Setting innerHTML as tab variable
-  document.getElementById("blk").innerHTML = tab;
-  document.getElementById("blk_tx").innerHTML = tab_tx;
-
-
-
-
-
-}
-
-
-
-function show(data) {
-  let tab = 
-      `<tr>
-        <th>Hash</th>
-        <th>Height</th>
-        <th>Chain</th>
-       </tr>`;
-  
-  // Loop to access all rows 
-  for (let r of data) {
-        tab += `
-        <tr> 
-          <td> <a href="?blk=${r.hash}"> ${r.hash}</a> </td>
-          <td>${r.header.height}</td>
-          <td>${r.header.chain}</td> 
-        </tr>`;
-  }
-  // Setting innerHTML as tab variable
-  document.getElementById("net").innerHTML = tab;
-
-}
-
-function show1(data) {
-    let tab = 
-    `<tr>
-    <th>Account</th>
-    <th>Balance</th>
-    <th>Smart contract</th>
-    </tr>
-`;
-
-        tab +=`
-          <tr> 
-            <td>${data.Result.address} </td>
-            <td>${data.Result.balance}</td>
-            <td>${data.Result.smart_contract}</td> 
-          </tr>`;
-    
-    // Setting innerHTML as tab variable
-    document.getElementById("acc").innerHTML = tab;
-  
-}
-function show2(data) {
-  let tab = 
-  `<tr>
-  <th>From</th>
-  <th>Amount</th>
-  <th>To</th>
-  <th>Hash</th>
-  </tr>
-`;
-
-      tab +=`
-        <tr> 
-          <td>${data.sender} </td>
-          <td>${data.amount}</td>
-          <td>${data.reciver}</td> 
-          <td>${data.hash}</td> 
-
-        </tr>`;
-  
-  // Setting innerHTML as tab variable
-  document.getElementById("tx").innerHTML = tab;
-
-}
-function search() {
-  // get option of what_to_search
-  let op = document.getElementById("what_to_search").option;
-  // get value of search_thing
-  let input = document.getElementById("search_thing").value;
-  // opens link wiht option and input
-  window.location.href = "/?" + op +  "=" + input;
-}
 function App() {
-    const windowUrl = window.location.search;
-    const params = new URLSearchParams(windowUrl);
-    let main = params.get("id");
-    let accpar = params.get("acc");
-    let blk = params.get("blk");
-    let tx = params.get("tx");
-
-    console.log(main);
-
-
-    if (accpar != null) { 
-        let getac = getacc(accpar);
-        console.log(getac);
-        return (
-            <div className="App">
-                  <h1>Redstone Network Account: {accpar}</h1>
- 
-                <a href="/">Go back</a>
-
-                <div class="center">
-                  <table class="table1" id="acc">
-                        Coult not find  {accpar}?
-                        Please try again!
-                </table>
-                </div>
-            </div>
-        );
-    }   
-    if (tx != null) {
-      let gettx1 = gettx(tx);
-      console.log(gettx);
-        return (
-            <div className="App">
-                  <h1>Redstone Network Transaction: {tx}</h1>
-                  <a href="/">Go back</a>
-                  <div class="center">
-                    <table class="table1" id="tx">
-                            Coult not find  {tx}?
-                            Please try again!
-                    </table>
-                  </div>
-            </div>
-        );
-    }
-    if (blk != null) { 
-      getblkhash(blk);
-      console.log(blk);
-      return (
-          <div className="App">
-                <h1>Redstone Network Block Hash: {blk}</h1>
-                <a href="/">Go back</a>
-                <div class="center">
-                <table class="table1" id="blk">
-                      Coult not find  {accpar}?
-                      Please try again!
-                </table>
-                </div>
-                <h1>Transactions</h1>
-                <table class="table1" id="blk_tx">
-                      Coult not find  {accpar}?
-                      Please try again!
-                </table>
-          </div>
-      );
-  }   
-
-    if (main == null) {
-    getblk(1);
-
-
-    return (
+  return (
     <div className="App">
-      <div class="topnav">
-        <a href="#about">Redstone Network Explorer</a>
-        <div class="search-container">
-          <form onsubmit="search()">
-              <input type="text" id="search_thing" placeholder="Search block hash" name="blk"/>
-              <button onclick="search()">Search</button>          </form>
-        </div>
-      </div>
-      
-        <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-one-quarter">
-                    <div class="column curve-box">
-                        <div class="curve-box-inner">
-                            <h1 class="subtitle is-4"><span id='supply'></span> RS</h1>
-                            <p>Total supply</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="columns">
-                <div class="column is-one-quarter">
-                    <div class="column curve-box">
-                        <div class="curve-box-inner">
-                            <h1 class="subtitle is-4"><span id='supply'></span>0</h1>
-                            <p>Chains</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="column is-one-quarter">
-                    <div class="column curve-box">
-                        <div class="curve-box-inner">
-                            <h1 class="subtitle is-4"><span id='supply'></span>0</h1>
-                            <p>Transactions</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <br></br>
-            <div class="column">
-                <div class="column curve-box">
-                    <div class="curve-box-inner">
-                        <table class="table" id="net">
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+      <header className="App-header">
+      <button onClick={CreateWallet}>Create Wallet</button>
+      <p>Address:</p>
+      <p id="wallet_address"></p>
+      <p>Private Key:</p>
+      <p id="wallet_private_key"></p>
+      <p>Public Key:</p>
+      <p id="wallet_public_key"></p>
+
+    </header>
     </div>
-    );
-    } 
+  );
+
+    
 }
 
 export default App;
